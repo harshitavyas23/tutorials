4b8afcabc43dc18a997dbbd1ea301a8b43b7c84c
harshitavyas23 <76699579+harshitavyas23@users.noreply.github.com>
2025-12-02 21:33:39 +0530

    Create script.js
    added script js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("helloBtn").addEventListener("click", function() {
+    document.getElementById("message").innerText = 
+        "🎉 Hello Harshita! This message is from JavaScript.";
+});
